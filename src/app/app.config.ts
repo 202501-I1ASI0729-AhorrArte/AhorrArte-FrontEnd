84649c317951c80d397c5d1b715c48ebb24f568e
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-07-09 17:42:02 -0500

    fix: fixed app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -36,7 +36,7 @@ export const appConfig: ApplicationConfig = {
           deps: [HttpClient]
         }
       })
-    ), provideAnimationsAsync()],
+    )],
 
 
 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(),provideHttpClient() ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
